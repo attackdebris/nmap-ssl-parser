3ca0e1a75d91480cee758b16a82d3e8aba7fbc57
root <attackdebris@gmail.com>
2017-06-22 19:50:19 +0100

    first public commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Nmap-ssl-parser 
+
+Nmap-ssl-parser is a python script designed to query nmap XML output and provide a list of usable ssl services in the format `host:port`
+
+The nmap-ssl-parser script parses an nmap.xml output file, extracts all SSL services and write them to a file.
+
+
+## Installation
+
+git clone https://github.com/attackdebris/nmap-ssl-parser.git
+
+### Prerequisites 
+
+The only pre-reqs is:
+
+1. You need a valid Nmap XML output file (see below)
+
+### Nmap XML File
+
+The Nmap XML file must have been created with version scanning enabled i.e. via Nmap flags `-sV` or `-A` (see below) 
+
+```bash
+nmap -A -p 1-65535 -iL targets.txt -oX nmap-output.xml 
+nmap -sS -sV -p 1-65535 -iL targets.txt -oX nmap-output.xml
+```
+
+## Usage
+
+```bash
+./nmap-ssl-parser.py 
+
+nmap-ssl-parser - v0.2 ( https://github.com/attackdebris/nmap-ssl-parser )
+
+USAGE: nmap-ssl-parser.py [nmap-ouput.xml] [outputfile]
+```
+
+### Example
+```bash
+./nmap-ssl-parser.py nmap-output.xml ssl-services.txt
+nmap-ssl-parser - v0.2 ( https://github.com/attackdebris/nmap-ssl-parser )
+
+Results saved to: ssl-services.txt
+```
+
+### Output / Results
+
+The output from the script is concatenated file (see below)
+
+```bash
+cat ssl-services.txt 
+185.176.90.16:443
+199.101.100.186:31337
+```
---
+++ nmap-ssl-parser.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+#
+# nmap-ssl-parser version 0.2
+#
+# https://github.com/attackdebris/auto-sslscan
+#
+# Base code credit: https://github.com/DanMcInerney/nmap-parser/blob/master/nmap-parser.py 
+#
+
+import argparse
+import subprocess
+import sys
+from libnmap.process import NmapProcess
+from libnmap.parser import NmapParser, NmapParserException
+ 
+instructions =  "nmap-ssl-parser - v0.2 ( https://github.com/attackdebris/nmap-ssl-parser )\n" +\
+                "\nUSAGE: nmap-ssl-parser.py [nmap-ouput.xml] [outputfile]"
+
+if len(sys.argv) <3 or sys.argv[1] == "-h" or sys.argv[1] == "--h" or sys.argv[1] == "-help" or sys.argv[1] == "--help":
+        print instructions
+        sys.exit()
+elif len(sys.argv) >3:
+	print instructions
+	sys.exit()
+elif len(sys.argv) ==3:
+	nmapxml = sys.argv[1]
+	myfile = sys.argv[2]
+	f = open(myfile, 'w')
+	f.close
+	print 'nmap-ssl-parser - v0.2 ( https://github.com/attackdebris/nmap-ssl-parser )\n'
+
+def parse_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument(sys.argv[1])
+	return parser.parse_args()
+
+def report_parser(report):
+    ''' Parse the Nmap XML report '''
+    for host in report.hosts:
+        ip = host.address
+
+            # Get the port and service
+            # objects in host.services are NmapService objects
+	for s in host.services:
+            # Check if port is open
+		if s.open():
+			serv = s.service
+                	port = s.port
+	        	tunnel = s.tunnel
+
+                	# Perform some action on the data
+                	print_data(ip, port, tunnel)
+
+def print_data(ip, port, tunnel):
+	''' Do something with the nmap data '''
+	if tunnel != '':
+		f = open(myfile, 'a+')
+		print >> f, '{0}:{1}'.format(ip,port) 
+		f.close
+
+def end():
+	print 'Results saved to: {}'.format(myfile)	
+
+def main():
+    report = NmapParser.parse_fromfile(nmapxml)
+    report_parser(report)
+    end()
+
+main()
